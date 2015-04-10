d7bc642fbebdbb570c12c5a26c397ab347f0558e
robomex <atmorales@gmail.com>
2015-04-09 23:46:53 -0500

    added search functionality and search scope buttons

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     var searchController: UISearchController!
 
+    var suggestedSearchFoods: [String] = []
+    var filteredSuggestedSearchFoods: [String] = []
+    
+    var scopeButtonTitles = ["Recommended", "Search Results", "Saved"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,9 +37,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         self.tableView.tableHeaderView = self.searchController.searchBar
         
+        
+        self.searchController.searchBar.scopeButtonTitles = scopeButtonTitles
         self.searchController.searchBar.delegate = self
         
         self.definesPresentationContext = true
+        
+        
+        self.suggestedSearchFoods = ["apple", "bacon", "bagel", "banana", "beer", "bread", "broccoli", "brussel sprouts", "burger", "cappuccino", "carrots", "cheese", "chicken nuggets", "chili", "chocolate chip cookies", "coffee", "donut", "graham cracker", "hot dog", "ice cream", "ketchup", "milk", "mountain dew", "nuts", "oatmeal", "orange juice", "peanut butter", "pizza", "pork belly", "potato", "pretzels", "rice", "salsa", "shrimp", "spaghetti", "wine"]
         
     }
 
@@ -45,19 +56,61 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // Mark - UITableViewDataSource
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if self.searchController.active {
+            return self.filteredSuggestedSearchFoods.count
+        } else {
+            return self.suggestedSearchFoods.count
+        }
+        
+        
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        
+        var foodName: String
+        
+        if self.searchController.active {
+            foodName = filteredSuggestedSearchFoods[indexPath.row]
+        } else {
+            foodName = suggestedSearchFoods[indexPath.row]
+        }
+        
+        cell.textLabel?.text = foodName
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+        
     }
+    
     
     
     // Mark - UISearchResultsUpdating
     
     func updateSearchResultsForSearchController(searchController: UISearchController) {
         
+        
+        let searchString = self.searchController.searchBar.text
+        let selectedScopeButtonIndex = self.searchController.searchBar.selectedScopeButtonIndex
+        
+        self.filterContentForSearch(searchString, scope: selectedScopeButtonIndex)
+        self.tableView.reloadData()
+        
+        
     }
+    
+    func filterContentForSearch (searchText: String, scope: Int) {
+        self.filteredSuggestedSearchFoods = self.suggestedSearchFoods.filter({ (food: String) -> Bool in
+            var foodMatch = food.rangeOfString(searchText)
+            return foodMatch != nil
+        })
+    }
+    
+    
 }
 
